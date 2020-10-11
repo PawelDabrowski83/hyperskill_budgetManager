2c877051515fe77e4e1874e2f7e16714fce39eb2
fluor <pawel.d.dabrowski@gmail.com>
2020-10-11 11:13:00 +0200

    Price has private constructor now

--- src/main/java/org/hyperskill/Price.java
+++ src/main/java/org/hyperskill/Price.java
@@ -1,12 +1,19 @@
 package org.hyperskill;
 
+import java.util.Objects;
+
 public class Price {
     public static final Price FREE = new Price(0, 0);
 
     private final int dollars;
     private final int cents;
 
-    public Price(int dollars, int cents) {
+    private Price(int dollars, int cents) {
+        this.dollars = dollars;
+        this.cents = cents;
+    }
+
+    public static Price build(int dollars, int cents){
         if (dollars < 0){
             dollars = 0;
         }
@@ -17,8 +24,7 @@ public class Price {
             cents -= 100;
             dollars++;
         }
-        this.dollars = dollars;
-        this.cents = cents;
+        return new Price(dollars, cents);
     }
 
     public int[] getValue(){
@@ -34,5 +40,27 @@ public class Price {
         }
         dollars += this.dollars + price.dollars;
         return new Price(dollars, cents);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price = (Price) o;
+        return dollars == price.dollars &&
+                cents == price.cents;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dollars, cents);
+    }
+
+    @Override
+    public String toString() {
+        return "Price{" +
+                "dollars=" + dollars +
+                ", cents=" + cents +
+                '}';
     }
 }
--- src/test/java/org/hyperskill/PriceTest.java
+++ src/test/java/org/hyperskill/PriceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PriceTest {
 
@@ -20,9 +21,23 @@ public class PriceTest {
     private static Stream<Arguments> getValueArgumentsProvider(){
         return Stream.of(
                 Arguments.of(new int[]{0, 0}, Price.FREE),
-                Arguments.of(new int[]{10, 19}, new Price(10, 19)),
-                Arguments.of(new int[]{0, 99}, new Price(0, 99)),
-                Arguments.of(new int[]{10, 0}, new Price(9, 100))
+                Arguments.of(new int[]{10, 19}, Price.build(10, 19)),
+                Arguments.of(new int[]{0, 99}, Price.build(0, 99)),
+                Arguments.of(new int[]{10, 0}, Price.build(9, 100))
+        );
+    }
+
+    @DisplayName("should addPrice() add two prices")
+    @ParameterizedTest
+    @MethodSource("addPriceArgumentsProvider")
+    void addPrice(Price expected, Price base, Price added){
+        assertEquals(expected, base.addPrice(added));
+    }
+    private static Stream<Arguments> addPriceArgumentsProvider(){
+        return Stream.of(
+                Arguments.of(Price.FREE, Price.FREE, Price.FREE),
+                Arguments.of(Price.build(1, 0), Price.build(0, 50), Price.build(0, 50)),
+                Arguments.of(Price.build(10, 12), Price.FREE, Price.build(10, 12))
         );
     }
 }
